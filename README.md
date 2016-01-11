cea30abc86fd83a96e3a95dcb47d9a68944ce207
Rangel Reale <rangelspam@gmail.com>
2016-01-11 16:45:28 -0200

    * Initial import

---
+++ README.md
@@ -0,0 +1,37 @@
+Url paramter handling for Yii2
+==============================
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist RangelReale/yii2-urlparam "*"
+```
+
+or add
+
+```json
+"RangelReale/yii2-urlparam": "*"
+```
+
+to the `require` section of your composer.json.
+
+
+The idea
+--------
+
+Url paramater handling helper.
+
+
+How to use
+----------
+
+
+Credits
+-------
+
+Rangel Reale (rangelspam@gmail.com)
---
+++ UrlParam.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace RangelReale\urlparam;
+
+use yii\base\Object;
+use yii\base\NotSupportedException;
+
+class UrlParam extends Object
+{
+    private $_params = [];
+    
+    public function addParam($name, $actions = [], $value = null)
+    {
+        $this->_params[$name] = [
+            'value' => $value,
+            'actions' => $actions,
+        ];
+    }
+    
+    public function setValue($name, $value = null)
+    {
+        if (!is_array($name))
+            $name = [$name => $value];
+        
+        foreach ($name as $n => $v) {
+            if (!isset($this->_params[$n]))
+                throw new NotSupportedException('Parameter '.$n.' not found');
+
+            $this->_params[$n]['value'] = $v;
+        }
+        return $this;
+    }
+    
+    public function url($action, $extraParams = [])
+    {
+        $ret = [$action];
+        foreach ($this->_params as $pname => $pvalue) {
+            $include = true;
+            if (is_null($pvalue['value']))
+                $include = false;
+            
+            if ($include && count($pvalue['actions']) > 0) {
+                $include = false;
+                foreach ($pvalue['actions'] as $paramAction) {
+                    $is_not = false;
+                    if (substr($paramAction, 0, 1) == '!') {
+                        $paramAction = substr($paramAction, 1);
+                        $is_not = true;
+                    }
+                    if ($paramAction == '@') {
+                        $include = !$is_not;
+                    }
+                    if ($action == $paramAction) {
+                        $include = !$is_not;
+                    }
+                }
+            }
+            
+            if ($include) {
+                $ret[$pname] = (string)$pvalue['value'];
+            }
+        }
+        foreach ($extraParams as $pname => $pvalue) {
+            if (is_null($pvalue)) {
+                if (isset($ret[$pname]))
+                    unset($ret[$pname]);
+            } else {
+                $ret[$pname] = (string)$pvalue;
+            }
+        }
+        return $ret;
+    }
+}
